8c1121fc883625ac223fbce0c2db3902c9f43f8e
Christian Farris <christianfarriscs@gmail.com>
2023-02-25 22:39:06 -0600

    feat: add completed projects via github

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,96 @@
-import fs from "fs";
-import playwright from "playwright";
+import fs from 'fs';
+import playwright from 'playwright';
+import { ofetch } from 'ofetch';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+type RepositoryTopic = {
+  topic: { name: string };
+};
+
+type GithubRepo = {
+  name: string;
+  repositoryTopics: {
+    nodes: RepositoryTopic[];
+  };
+};
+
+const GITHUB_QUERY = `query {
+  user(login: "chrstnfrrs") {
+      repositories (first: 100) {
+          nodes {
+              id
+              name
+              homepageUrl
+              url
+              description
+              repositoryTopics (first: 100) {
+                  nodes {
+                      topic {
+                          name
+                      }
+                  }
+              }
+          }
+      }
+  }
+}`;
+
+console.log('------------------------------------');
+console.log('Fetching repository list from Github');
+console.log('------------------------------------\n\n');
+
+const { data: repos } = await ofetch('https://api.github.com/graphql', {
+  method: 'POST',
+  headers: {
+    Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+  },
+  body: {
+    query: GITHUB_QUERY,
+  },
+});
+
+const projects: GithubRepo[] = repos.user.repositories.nodes.filter(
+  (repo: GithubRepo) =>
+    repo.repositoryTopics.nodes.some(
+      (repoTopic: RepositoryTopic) => repoTopic.topic.name === 'frontend-mentor'
+    )
+);
+
+console.log('-----------------------------------------');
+console.log('Scraping the frontend mentor project list');
+console.log('-----------------------------------------\n\n');
 
 const browser = await playwright.chromium.launch({
   headless: false, // setting this to true will not run the UI
 });
 
 const page = await browser.newPage();
-await page.goto("https://www.frontendmentor.io/challenges");
+await page.goto('https://www.frontendmentor.io/challenges');
 
-const listItems = await page.getByRole("listitem").all();
+const listItems = await page.getByRole('listitem').all();
 
-const data = [];
+const data = [['Name', 'Difficulty', 'Skills', 'Completed']];
 
 for (const li of listItems) {
-  const heading = await li.getByRole("heading");
-  const link = await heading.getByRole("link");
+  const heading = await li.getByRole('heading');
+  const link = await heading.getByRole('link');
   const name = (await link.allInnerTexts())[0];
 
   if (name) {
-    const skillsEls = await li.getByRole("listitem").all();
+    const skillsEls = await li.getByRole('listitem').all();
     const skills = [];
     for (const skill of skillsEls) {
       skills.push((await skill.allInnerTexts())[0]);
     }
 
-    let difficulty = "";
+    let difficulty = '';
     for (const level of [
-      "NEWBIE",
-      "JUNIOR",
-      "INTERMEDIATE",
-      "ADVANCED",
-      "GURU",
+      'NEWBIE',
+      'JUNIOR',
+      'INTERMEDIATE',
+      'ADVANCED',
+      'GURU',
     ]) {
       const texts = await li.getByText(level).allInnerTexts();
       if (texts.some((text) => text === level)) {
@@ -39,28 +99,44 @@ for (const li of listItems) {
       }
     }
 
-    data.push({ name, skills, difficulty });
+    data.push([
+      name.replace(/,/g, ''),
+      difficulty,
+      skills.join(' '),
+      projects.some((project) =>
+        project.name
+          .replace(/-/g, ' ')
+          .toLocaleLowerCase()
+          .includes(
+            name.replace(/,/g, '').replace(/-/g, ' ').toLocaleLowerCase()
+          )
+      )
+        ? 'Yes'
+        : 'No',
+    ]);
   }
 }
 
 await browser.close();
 
-const csvData = [["Name", "Difficulty", "Skills"]];
-data.forEach((project) => {
-  csvData.push([
-    project.name.replace(/,/g, ""),
-    project.difficulty,
-    project.skills.join(" "),
-  ]);
-});
-const csv = csvData.map((row) => row.join(", ")).join("\n");
+const csv = data.map((row) => row.join(', ')).join('\n');
 
-fs.writeFile("frontend-mentor-projects.csv", csv, "utf8", function (err) {
+fs.writeFile('frontend-mentor-projects.csv', csv, 'utf8', function (err) {
   if (err) {
     console.log(
-      "Some error occured - file either not saved or corrupted file saved."
+      'Some error occured - file either not saved or corrupted file saved.'
     );
   } else {
-    console.log("frontend-mentor-projects.csv saved!");
+    console.log('------------------------------------');
+    console.log('frontend-mentor-projects.csv saved!');
+    console.log('------------------------------------\n\n');
+
+    console.log('------------------------------------');
+    console.log(
+      `You have completed ${
+        data.filter((project) => project[3] === 'Yes').length
+      } / ${data.length - 1} projects!`
+    );
+    console.log('------------------------------------');
   }
 });
